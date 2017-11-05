04724d8e9128b386614214f6381e4c06aef01933
Glitch (empty-phone) <none>
2017-11-05 04:05:34 +0000

    🌎🎉 Updated with Glitch

---
+++ skills/menu.js
@@ -0,0 +1,122 @@
+/*
+
+Botkit Studio Skill module to enhance the "Menu" script
+
+*/
+
+
+module.exports = function(controller) {
+    // define a before hook
+    // you may define multiple before hooks. they will run in the order they are defined.
+    // See: https://github.com/howdyai/botkit/blob/master/docs/readme-studio.md#controllerstudiobefore
+    controller.studio.before('Menu', function(convo, next) {
+
+        // do some preparation before the conversation starts...
+        // for example, set variables to be used in the message templates
+        // convo.setVar('foo','bar');
+
+        console.log('BEFORE: Menu');
+        // don't forget to call next, or your conversation will never continue.
+        next();
+
+    });
+
+    /* Validators */
+    // Fire a function whenever a variable is set because of user input
+    // See: https://github.com/howdyai/botkit/blob/master/docs/readme-studio.md#controllerstudiovalidate
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+    // Validate user input: question_1
+    controller.studio.validate('Menu','question_1', function(convo, next) {
+
+        var value = convo.extractResponse('question_1');
+
+        // test or validate value somehow
+        // can call convo.gotoThread() to change direction of conversation
+
+        console.log('VALIDATE: Menu VARIABLE: question_1');
+
+        // always call next!
+        next();
+
+    });
+
+    // Validate user input: question_2
+    controller.studio.validate('Menu','question_2', function(convo, next) {
+
+        var value = convo.extractResponse('question_2');
+
+        // test or validate value somehow
+        // can call convo.gotoThread() to change direction of conversation
+
+        console.log('VALIDATE: Menu VARIABLE: question_2');
+
+        // always call next!
+        next();
+
+    });
+
+    // Validate user input: question_3
+    controller.studio.validate('Menu','question_3', function(convo, next) {
+
+        var value = convo.extractResponse('question_3');
+
+        // test or validate value somehow
+        // can call convo.gotoThread() to change direction of conversation
+
+        console.log('VALIDATE: Menu VARIABLE: question_3');
+
+        // always call next!
+        next();
+
+    });
+
+    /* Thread Hooks */
+    // Hook functions in-between threads with beforeThread
+    // See: https://github.com/howdyai/botkit/blob/master/docs/readme-studio.md#controllerstudiobeforethread
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+    // Before the default thread starts, run this:
+    controller.studio.beforeThread('Menu','default', function(convo, next) {
+
+        /// do something fun and useful
+        // convo.setVar('name','value');
+
+        console.log('In the script *Menu*, about to start the thread *default*');
+
+        // always call next!
+        next();
+    });
+
+    // Before the on_timeout thread starts, run this:
+    controller.studio.beforeThread('Menu','on_timeout', function(convo, next) {
+
+        /// do something fun and useful
+        // convo.setVar('name','value');
+
+        console.log('In the script *Menu*, about to start the thread *on_timeout*');
+
+        // always call next!
+        next();
+    });
+
+
+    // define an after hook
+    // you may define multiple after hooks. they will run in the order they are defined.
+    // See: https://github.com/howdyai/botkit/blob/master/docs/readme-studio.md#controllerstudioafter
+    controller.studio.after('Menu', function(convo, next) {
+
+        console.log('AFTER: Menu');
+
+        // handle the outcome of the convo
+        if (convo.successful()) {
+
+            var responses = convo.extractResponses();
+            // do something with the responses
+
+        }
+
+        // don't forget to call next, or your conversation will never properly complete.
+        next();
+    });
+}
