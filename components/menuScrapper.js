a18d264d1c239716e0a89a7297a20733a55e33fa
Orest Hazda <orest@crowdlinker.com>
2017-11-14 22:21:44 -0500

    Add attr to puppeter to make it run on Heroku.

--- components/menuScrapper.js
+++ components/menuScrapper.js
@@ -1,7 +1,9 @@
 const puppeteer = require('puppeteer');
 
 async function getMenuImage() {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
     let fileName = '';
 
