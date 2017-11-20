3d1c80777f738818ed74845ecca7a4b1b2992a20
Orest Hazda <orest@crowdlinker.com>
2017-11-19 19:52:56 -0500

    Add "menu list" skill.

--- components/menuScrapper.js
+++ components/menuScrapper.js
@@ -3,19 +3,11 @@ const puppeteer = require('puppeteer');
 async function getMenuImage() {
     try {
 
-        const browser = await puppeteer.launch({
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        });
-        const page = await browser.newPage();
+        const browser = await openBrowser();
+        const page = await openPage(browser);
         let fileName = '';
     
-        page.setViewport({
-            width: 1280,
-            height: 800,
-            deviceScaleFactor: 2,
-        });
-    
-        await page.goto('https://yumba.ca/#/on-the-menu');
+        
         const menuElement = await page.$('.meals-grid-wrapper');
         const navigation = await page.$eval('.navbar', el => {
             el.style.display = 'none';
@@ -24,7 +16,6 @@ async function getMenuImage() {
         const container = await page.$eval('.section', el => {el.style.padding = '20px'});
         
     
-        console.log("Create screen shot");
         fileName = `menu-${new Date().getTime()}.jpeg`;
         await menuElement.screenshot({
             path: `./public/images/menu/${fileName}`,
@@ -32,8 +23,8 @@ async function getMenuImage() {
             type: 'jpeg'
         });
     
-        await browser.close().then(() => {
-            console.log('closed');
+        await closeBrowser(browser, () => {
+            console.log('Page closed.');
         });
     
         return fileName;
@@ -43,6 +34,67 @@ async function getMenuImage() {
     }
 };
 
+async function getMenuList() {
+    try {
+        const browser = await openBrowser();
+        const page = await openPage(browser);
+        
+        const result = await page.evaluate(() => {
+            let data = []; // Create an empty array that will store our data
+            let elements = document.querySelectorAll('#entreeMeals .meal-name span'); // Select all Products
+    
+            for (var element of elements){ // Loop through each proudct
+                let name = element.innerText;
+                data.push(name); // Push an object with the data onto our array
+            }
+    
+            return data; // Return our data array
+        });
+    } catch (error) {
+        console.log(`Error: When getting image list. (${error})`);
+    }
+}
+
+async function openBrowser() {
+    try {
+        return await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
+    } catch(e) {
+        console.log('Error: Cant open browser');
+    }
+}
+
+async function openPage(browser) {
+    try {
+        const page = await browser.newPage();
+        
+        page.setViewport({
+            width: 1280,
+            height: 800,
+            deviceScaleFactor: 2,
+        });
+    
+        await page.goto('https://yumba.ca/#/on-the-menu');
+    
+        return page;
+
+    } catch (error) {
+        console.log('Error: Cant open the page.');
+    }
+}
+
+async function closeBrowser(browser, callback) {
+    try {
+        await browser.close().then(() => {
+            callback();
+        });    
+    } catch (error) {
+        console.log('Error: Cant close the browser');
+    }
+}
+
 module.exports = {
     getMenuImage: getMenuImage,
+    getMenuList: getMenuList,
 }
